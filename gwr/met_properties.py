2c6bf3b3b46ecc1b0079f1bf941beacd231d7407
Hose-Soh <sohrawardy.mhose@gmail.com>
2023-05-20 15:37:06 +0600

    .

---
+++ gwr/met_properties.py
@@ -0,0 +1,56 @@
+import ee
+from gwr import ee_utils
+
+
+def get_precipitation_data_for_dates(start_date, end_date):
+    return (
+        ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY")
+            .select("precipitation")
+            .filterDate(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
+    )
+
+
+def get_potential_evaporation_for_dates(start_date, end_date):
+    # Import potential evaporation PET and its quality indicator ET_QC.
+    return (
+        ee.ImageCollection("MODIS/006/MOD16A2")
+            .select(["PET", "ET_QC"])
+            .filterDate(start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
+    )
+
+
+def get_mean_monthly_meteorological_data(start_date, end_date):
+    """
+    Returns an ImageCollection that combines the Precipitation and Potential Evaporation data
+    for a region across a time period resampled to provide monthly mean values
+    """
+    pr = get_precipitation_data_for_dates(start_date, end_date)
+    pet = get_potential_evaporation_for_dates(start_date, end_date)
+
+    # Apply the resampling function to the precipitation dataset.
+    pr_m = ee_utils.sum_resampler(pr, 1, "month", 1, "pr")
+
+    # Apply the resampling function to the PET dataset.
+    pet_m = ee_utils.sum_resampler(pet.select("PET"), 1, "month", 0.0125, "pet")
+
+    # Combine precipitation and evapotranspiration.
+    meteo = pr_m.combine(pet_m)
+
+    return meteo
+
+
+def get_mean_monthly_meteorological_data_for_roi_df(roi, scale, meteoImageCollection, ):
+    # Import meteorological data as an array at the location of interest.
+    meteo_arr = meteoImageCollection.getRegion(roi, scale).getInfo()
+
+    # Transform the array into a pandas dataframe and sort the index.
+    # Data for ROI may have multiple sample points within ROI for a date so group by date and take the mean
+    # To avoid loosing the datetime and time fields (used elswhere), group by both then remove time from the index
+    meteo_df = ee_utils.ee_array_to_df(meteo_arr, ["pr", "pet"]).groupby(['datetime', 'time']).mean(
+        ['pr', 'pet']).sort_values("datetime")
+    meteo_df.reset_index(level='time', inplace=True)
+    meteo_df.rename(columns={'pr': 'mean-pr'}, inplace=True)
+    meteo_df.rename(columns={'pet': 'mean-pet'}, inplace=True)
+    meteo_df["date"] = meteo_df.index.strftime("%m-%Y")
+
+    return meteo_df
---
+++ gwr/soil_properties.py
@@ -0,0 +1,88 @@
+import ee
+import logging
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def convert_orgc_to_orgm(org_c):
+    ''' 
+        Converts organic carbon content into organic matter content.
+    '''
+    return org_c.multiply(1.724)
+
+
+def get_soil_prop(soil_type):
+    """
+    This function returns soil properties image
+    param (str): must be one of:
+        "sand"     - Sand fraction
+        "clay"     - Clay fraction
+        "orgc"     - Organic Carbon fraction
+    """
+    if soil_type == "sand":  # Sand fraction [%w]
+        snippet = "OpenLandMap/SOL/SOL_SAND-WFRACTION_USDA-3A1A1A_M/v02"
+        # Define the scale factor in accordance with the dataset description.
+        scale_factor = 1 * 0.01
+
+    elif soil_type == "clay":  # Clay fraction [%w]
+        snippet = "OpenLandMap/SOL/SOL_CLAY-WFRACTION_USDA-3A1A1A_M/v02"
+        # Define the scale factor in accordance with the dataset description.
+        scale_factor = 1 * 0.01
+
+    elif soil_type == "orgc":  # Organic Carbon fraction [g/kg]
+        snippet = "OpenLandMap/SOL/SOL_ORGANIC-CARBON_USDA-6A1C_M/v02"
+        # Define the scale factor in accordance with the dataset description.
+        scale_factor = 5 * 0.001  # to get kg/kg
+    else:
+        logger.error(f"The soil property '{soil_type} was not recognised")
+        return None
+
+    # Apply the scale factor to the ee.Image.
+    dataset = ee.Image(snippet).multiply(scale_factor)
+
+    return dataset
+
+
+def get_local_soil_profile_at_poi(dataset, roi, buffer, olm_bands):
+    # # Soil depths [in cm] where we have data.
+    # olm_depths = [0, 10, 30, 60, 100, 200]
+
+    # # Names of bands associated with reference depths.
+    # olm_bands = ["b" + str(sd) for sd in olm_depths]
+    # Get properties at the location of interest and transfer to client-side.
+    prop = dataset.sample(roi, buffer).select(olm_bands).getInfo()
+
+    # Initialize an empty list to store dictionaries for each ID
+    data_dicts = []
+
+    # Iterate over each feature and extract 'id' and 'properties'
+    for feature in prop['features']:
+        id_value = feature['id']
+        properties = feature['properties']
+
+        # Create a dictionary for each ID
+        data_dict = {'id': id_value}
+        data_dict.update(properties)
+
+        # Append the dictionary to the list
+        data_dicts.append(data_dict)
+
+    # Create the DataFrame from the list of dictionaries
+    df = pd.DataFrame(data_dicts)
+
+    # Reorder the columns
+    column_order = ['id', 'b0', 'b10', 'b30', 'b60', 'b100', 'b200']
+    df = df[column_order]
+
+    # Calculate the average of 'b0', 'b10', 'b30', 'b60', 'b100', 'b200'
+    averages = df[['b0', 'b10', 'b30', 'b60', 'b100', 'b200']].mean()
+
+    # Create a dictionary with 'b0', 'b10', 'b30', 'b60', 'b100', 'b200' as keys and their average as values
+    average_dict = averages.to_dict()
+
+    # Selection of the features/properties of interest.
+    # Re-shaping of the dict.
+    profile = {key: round(val, 3) for key, val in average_dict.items()}
+    return profile
